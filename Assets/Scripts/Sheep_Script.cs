cd30378ae4ef0a2b8e5e89a54f1a57ec60e8ae13
SergiuGlingeanu <s.glingeanu@gmail.com>
2020-09-24 10:17:43 +0100

    Sheep kinda working

---
+++ Assets/Scripts/Sheep_Script.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep_Script : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    private Vector2 _destination;
+    private Vector2 _direction, _position;
+
+    private Transform _player;
+
+    private bool _scared, _gotDestination;
+
+    public float sheepSpeed;
+    public float scareDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.Find("Player").GetComponent<Transform>();
+
+        _rb = GetComponent<Rigidbody2D>();
+        GetDestination();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, _destination) < 0.5)
+        {
+            GetDestination();
+        }
+
+        if (Vector2.Distance(transform.position, _player.position) < scareDistance)
+        {
+            _scared = true;
+            _gotDestination = false;
+            _direction = transform.position - _player.position;
+            _rb.velocity = _direction.normalized * sheepSpeed;
+        }
+        if (Vector2.Distance(transform.position, _player.position) >= scareDistance)
+        {
+            _scared = false;
+            GetDestination();
+        }
+    }
+
+    private void GetDestination()
+    {
+        if (!_scared && !_gotDestination)
+        {
+            _destination = new Vector2(Random.Range(-9, 9), Random.Range(-5, 5));
+
+            _position = new Vector2(transform.position.x, transform.position.y);
+
+            _direction = _destination - _position;
+
+            _rb.velocity = _direction.normalized * sheepSpeed;
+
+            _gotDestination = true;
+        }
+    }
+
+    private void RunFromPlayer()
+    {
+        _direction = transform.position - _player.position;
+        _rb.velocity = _direction.normalized * sheepSpeed;
+    }
+}
