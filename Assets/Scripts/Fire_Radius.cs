bec36d27ea092e8eae62824f8614a7d622f5ae8a
Kevin <61233166+kevinoh45TuD@users.noreply.github.com>
2020-09-27 21:43:40 +0100

    Range.2

---
+++ Assets/Scripts/Fire_Radius.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Fire_Radius : MonoBehaviour
+{
+    [Range(0, 180)] public int segments = 180;
+    // segment amount of circle
+    [Range(0, 10)] public float xRadius = 10;
+    
+    [Range(0, 10)] public float yRadius = 10;
+    // x and y radius, should remain the same
+    [Range(0, 5)] public float lineWidth = 1;
+    // width of the circle's line
+    LineRenderer line;
+
+    void Start()
+    {
+        line = gameObject.GetComponent<LineRenderer>();
+        line.SetWidth(lineWidth, lineWidth);
+        line.SetVertexCount(segments + 1);
+        line.useWorldSpace = false;
+       
+        
+        CreateCircle();
+
+    }
+
+    void CreateCircle()
+    {
+        float x;
+        float y;
+
+        float angle = 20f;
+
+        for (int i = 0; i < (segments + 1); i++)
+        {
+            x = Mathf.Sin(Mathf.Deg2Rad * angle) * xRadius;
+            y = Mathf.Cos(Mathf.Deg2Rad * angle) * yRadius;
+
+            line.SetPosition(i, new Vector3(x, y, 0));
+
+            angle += (360f / segments);
+            
+        }
+
+        
+    }
+}
