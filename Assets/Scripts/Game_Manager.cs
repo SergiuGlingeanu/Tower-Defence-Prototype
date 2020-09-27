0016b9c0ce2efb7b23c6c41d2403ab6c4ec55498
SergiuGlingeanu <s.glingeanu@gmail.com>
2020-09-27 12:14:31 +0100

    Goblins Spawn

---
+++ Assets/Scripts/Game_Manager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Manager : MonoBehaviour
+{
+
+    public GameObject goblin;
+    
+    void Start()
+    {
+        Instantiate(goblin, new Vector3(-1, 0, 0), Quaternion.identity);
+
+        Invoke("SpawnGoblins", 2);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void SpawnGoblins()
+    {
+        Instantiate(goblin, new Vector3(-1, 0, 0), Quaternion.identity);
+
+        Invoke("SpawnGoblins", 2);
+    }
+}
---
+++ Assets/Scripts/Goblin_Script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goblin_Script : MonoBehaviour
+{
+
+    public float health = 100;
+    public float speed = 2;
+    public float damage = 2;
+    public float attackCooldown = 2;
+
+    private Rigidbody2D _rb;
+    
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+
+        _rb.velocity = new Vector3(1, 0, 0);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
