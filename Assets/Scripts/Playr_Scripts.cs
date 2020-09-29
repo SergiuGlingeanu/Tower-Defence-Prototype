dd219957ca93c32fe0ea29748c51ac32d7738dc4
NiamhG <44605623+NiamhG42@users.noreply.github.com>
2020-09-29 17:02:41 +0100

    Dog running sounds
    Dog running noises in (only play when dog moves)

--- Assets/Scripts/Playr_Scripts.cs
+++ Assets/Scripts/Playr_Scripts.cs
@@ -8,15 +8,58 @@ public class Playr_Scripts : MonoBehaviour
     private Rigidbody2D _rb;
 
     public float playerSpeed;
+    private AudioSource dogAudio;
+    private bool dogMoving;
+    public bool m_Play;
+    public bool m_ToggleChange;
+    public bool dogIsMoving;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+
+        dogAudio = GetComponent<AudioSource>();
     }
 
     
     void Update()
     {
         _rb.velocity = new Vector2(Input.GetAxis("Horizontal") * playerSpeed, Input.GetAxis("Vertical") * playerSpeed);
+
+        playDogSounds();
+    }
+
+    public void playDogSounds()
+    {
+        if (Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical")) //Dog started running
+        {
+            dogIsMoving = true;
+            m_Play = true;
+            m_ToggleChange = true;
+        }
+
+        if (!(Input.GetButton("Horizontal") || Input.GetButton("Vertical"))) //Dog isn't moving
+        {
+            dogIsMoving = false;
+            m_Play = false;
+            m_ToggleChange = true;
+        }
+
+        //Check to see if you just set the toggle to positive
+        if (m_Play == true && m_ToggleChange == true)
+        {
+            //Play the audio you attach to the AudioSource component
+            dogAudio.Play();
+            //Ensure audio doesn’t play more than once
+            m_ToggleChange = false;
+        }
+        //Check if you just set the toggle to false
+        if (m_Play == false && m_ToggleChange == true)
+        {
+            //Stop the audio
+            dogAudio.Stop();
+            //Ensure audio doesn’t play more than once
+            m_ToggleChange = false;
+        }
     }
 }
