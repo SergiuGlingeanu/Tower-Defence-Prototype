f392d929004dea20b40797a09b9850c4f7adc6cd
Kevin <61233166+kevinoh45TuD@users.noreply.github.com>
2020-09-29 15:51:45 +0100

    Base Damage and Game Over!
    If base is hit by goblin it loses set amount.
    
    At half or lower health the base changes sprite.
    
    At no health a game over screen appears. (needs more work)...

---
+++ Assets/Scripts/Base_Hit.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Base_Hit : MonoBehaviour
+{
+    public Sprite[] baseSprite;
+
+    [Range(0, 50)] public int baseHealthStart;
+
+    private int baseHealth;
+
+    [Range(0, 10)] public int damageAm;
+
+    public bool baseDead;
+
+    public bool baseHalf;
+
+    public Text healthCount;
+
+    public GameObject dieUI;
+
+    public GameObject healthUI;
+
+    void Start()
+    {
+        baseHealth = baseHealthStart;
+    }
+
+    void Update()
+    {
+        healthCount.text = baseHealth.ToString();
+
+        if (baseHealth <= baseHealthStart / 2)
+        {
+            baseHalf = true;
+        }
+        
+        if (baseHealth <= 0)
+        {
+            baseDead = true;
+        }
+
+        if (baseHalf == true)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = baseSprite[0];
+        }
+        
+        if (baseDead == true)
+        {
+            dieUI.SetActive(true);
+            
+            healthUI.SetActive(false);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            baseHealth -= damageAm;
+            
+            Destroy(collision.gameObject);
+            
+        }
+    }
+}
