cf26734bbaa85e09441b0f8c5fee9d08deb1eddc
SergiuGlingeanu <s.glingeanu@gmail.com>
2020-09-29 11:15:52 +0100

    he he he yup

--- Assets/Scripts/Goblin_Script.cs
+++ Assets/Scripts/Goblin_Script.cs
@@ -11,13 +11,11 @@ public class Goblin_Script : MonoBehaviour
     public float attackCooldown = 2;
     public GameObject bloodSplatter;
 
-    private Rigidbody2D _rb;
+    private Vector3 _direction;
     
     void Start()
     {
-        _rb = GetComponent<Rigidbody2D>();
-
-        _rb.velocity = new Vector3(1, 0, 0);
+        
     }
 
     
@@ -32,6 +30,29 @@ public class Goblin_Script : MonoBehaviour
             Destroy(splatterEffect, totalDuration);
 
             Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Turn 1" || collision.gameObject.name == "Turn 3")
+        {
+            _direction = new Vector3(1 * speed * Time.deltaTime, 0, 0);
+        }
+
+        if (collision.gameObject.name == "Turn 2" || collision.gameObject.name == "Turn 6")
+        {
+            _direction = new Vector3(0, 1 * speed * Time.deltaTime, 0);
+        }
+
+        if (collision.gameObject.name == "Turn 4")
+        {
+            _direction = new Vector3(0, -1 * speed * Time.deltaTime, 0);
+        }
+
+        if (collision.gameObject.name == "Turn 5")
+        {
+            _direction = new Vector3(-1 * speed * Time.deltaTime, 0, 0);
         }
     }
 }
