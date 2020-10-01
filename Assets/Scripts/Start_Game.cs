7fcc8153c9753718e8bcc66c4264c89d0486671c
Kevin <61233166+kevinoh45TuD@users.noreply.github.com>
2020-10-01 23:10:52 +0100

    Start Screen

---
+++ Assets/Scripts/Start_Game.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Start_Game : MonoBehaviour
+{
+    public Button start;
+
+    public GameObject startButton;
+
+    void Start()
+    {
+        
+        start.OnClick.AddListener(startGame);
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("Sergiu's Scene");
+    }
+}
